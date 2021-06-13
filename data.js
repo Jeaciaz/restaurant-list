c6fa5ddf1ad34970b4571d20c834d0ef21f5b90e
Jeaciaz <kniazevs.v@yandex.ru>
2021-06-13 17:32:23 +0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,251 @@
+function getData() {
+  return [
+    // 1
+    {
+      name: 'Кафе Март — вино, еда, мозайки',
+      comment: 'Место с милой верандой, где хорошо собраться компанией',
+      href: 'https://www.instagram.com/cafemart25',
+      subway: 'Пушкинская',
+    },
+    {
+      name: 'Горыныч — мое почтение! 7 место из топ 100 в России',
+      comment: 'Место с милой верандой, где хорошо собраться компанией',
+      href: 'https://www.instagram.com/gorynich.moscow',
+      subway: 'Цветной б-р',
+    },
+    {
+      name: 'Падре Берлускони — новое место, классная веранда',
+      comment: 'Место с милой верандой, где хорошо собраться компанией',
+      href: 'https://www.instagram.com/padre_berlusconi',
+      subway: 'Цветной б-р',
+    },
+    {
+      name: 'Санта Базилия — маленькая уютная Италия в центре Москвы',
+      comment: 'Место с милой верандой, где хорошо собраться компанией',
+      href: 'https://www.instagram.com/allsaints_cafe',
+      subway: 'пр-т Мира',
+    },
+    {
+      name: 'ДиДи — грузинско-европейская кухня, душевно, уютно',
+      comment: 'Место с милой верандой, где хорошо собраться компанией',
+      href: 'https://instagram.com/didirestaurant',
+      subway: 'Тверская/Пушкинская',
+    },
+    {
+      name: 'Одесса-мама — просторно, с еврейским духом, вид на сад',
+      comment: 'Место с милой верандой, где хорошо собраться компанией',
+      href: 'https://instagram.com/cafeodessamama',
+      subway: 'пр-т Мира',
+    },
+    {
+      name: 'Ткемали — вкусная Грузия, шедевральные лампы',
+      comment: 'Место с милой верандой, где хорошо собраться компанией',
+      href: 'https://www.instagram.com/tkemalirest',
+      subway: 'Лубянка/Китай город',
+    },
+
+    // 2
+    {
+      name: 'Lambic — отличная сеть, но не из бюджетных',
+      href: 'https://5.lambic.ru/',
+      comment: 'Подороже',
+      subway: 'пр-т Мира',
+    },
+    {
+      name: 'Food Embassy — уютно',
+      href: 'https://www.instagram.com/food_embassy',
+      comment: 'Подороже',
+      subway: 'пр-т Мира',
+    },
+
+    // 3
+    {
+      name: 'Мишка-водка-балалайка — атмосферный русский дух',
+      comment: 'Без веранды, но тоже очень хорошо для большой компании',
+      href: 'https://www.instagram.com/mishka.balalaika',
+      subway: 'Таганская',
+    },
+
+    // 4
+    {
+      name: 'Оттепель — моя безграничная любовь с первого взгляда',
+      comment: 'С радостью вернёмся',
+      href: 'https://www.instagram.com/ottepel_vdnh',
+      subway: 'ВДНХ',
+    },
+    {
+      name:
+        'Московское небо — побратим Оттепели, но с другим меню и настроением, эдакая пост-советская аристократичность',
+      comment: 'С радостью вернёмся',
+      href: 'https://www.instagram.com/mosnebo',
+      subway: 'ВДНХ',
+    },
+    {
+      name:
+        'Sempre – еще она моя любовь в формате джунглей. Вкусно, красиво, но надо бронировать скамьи со спинками)',
+      comment: 'С радостью вернёмся',
+      href: 'https://www.instagram.com/sempre.moscow',
+      subway: 'Тверская/Пушкинская',
+    },
+    {
+      name: 'Техникум – gastrobistro',
+      comment: 'С радостью вернёмся',
+      href: 'https://www.instagram.com/tehnikumbistro',
+      subway: 'Тверская/Театральная',
+    },
+    {
+      name: 'Пицца 22 см – вкусно, но шумненько',
+      comment: 'С радостью вернёмся',
+      href: 'https://www.instagram.com/pizza22msk',
+      subway: 'Китай-Город',
+    },
+    {
+      name: 'Jpan – очень приятное маленькое японское бистро',
+      comment: 'С радостью вернёмся',
+      href: 'https://www.instagram.com/jpan.moscow/',
+      subway: 'Цветной бульвар',
+    },
+    {
+      name: 'Горожане – gastrobar',
+      comment: 'С радостью вернёмся',
+      href: 'https://www.instagram.com/gorozhane_gastrobar',
+      subway: 'Киевская/Парк культуры',
+    },
+    {
+      name: 'Hands – нео-бистро – стильно, продумано, с любовью',
+      comment: 'С радостью вернёмся',
+      href: 'https://www.instagram.com/hands.restaurant',
+      subway: 'Киевская/Парк культуры',
+    },
+    {
+      name: 'Kaif – роскошно, как Рычард Сапогоуф',
+      comment: 'С радостью вернёмся',
+      href: 'https://www.instagram.com/kaif.provenance',
+      subway: 'Киевская/Парк культуры',
+    },
+
+    // 5
+    {
+      name: 'Эларджи – шикарная Грузия',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://www.instagram.com/elargi_rest/',
+      subway: 'Кропоткинская',
+    },
+    {
+      name: 'Тифлисский дворик',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://www.instagram.com/tiflisskiy_dvorik_/',
+      subway: 'Кропоткинская',
+    },
+    {
+      name: '8oz – в сердце парка Горького, хвалят друзья',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://www.instagram.com/bar8oz/',
+      subway: 'Парк культуры',
+    },
+    {
+      name: 'Bjorn – Северная кухня / нестандартно!',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://www.instagram.com/bjorn.restaurant',
+      subway: 'Новокузнецкая/Третьяковская',
+    },
+    {
+      name: 'Па-Паэлья – Испанская',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://www.instagram.com/papaella_cafe',
+      subway: 'Сретенский/Лубянка',
+    },
+    {
+      name: 'Пробка – Итальянская',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://www.instagram.com/probkanatsvetnom',
+      subway: 'Цветной б-р',
+    },
+    {
+      name: 'Scrocchiarella – Итальянская',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://www.instagram.com/scrocchiarella',
+      subway: 'Китай-Город',
+    },
+    {
+      name: 'Vivians на лубянке',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://instagram.com/vivians_cafe',
+      subway: 'Китай-Город',
+    },
+    {
+      name: 'Lola',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://instagram.com/lola.moscow',
+      subway: 'Белорусская',
+    },
+    {
+      name: 'Kalabasa - кондитерская с кухней',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://instagram.com/kalabasa',
+      subway: 'Бауманская',
+    },
+    {
+      name: 'Северяне',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://instagram.com/severyane.moscow',
+      subway: 'Арбатская/Охотный ряд',
+    },
+    {
+      name: 'Крабы кутабы',
+      comment: 'Неизведанное, но манящее',
+      href: 'http://crabykutaby.ru/menu',
+      subway: 'Белорусская',
+    },
+    {
+      name: 'Салют',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://www.instagram.com/salutecoffee',
+      subway: 'Пушкинская',
+    },
+    {
+      name: 'КМ20',
+      comment: 'Неизведанное, но манящее',
+      href: 'https://www.instagram.com/km20food',
+      subway: 'Охотный ряд',
+    },
+
+    // 6
+    {
+      name: 'Twins Garden',
+      comment: 'Лига дорогобогато/когда-нибудь попробовать',
+      href: 'https://instagram.com/twinsgardenmoscow',
+      subway: 'Пушкинская',
+    },
+    {
+      name: 'White Rabbit',
+      comment: 'Лига дорогобогато/когда-нибудь попробовать',
+      href: 'https://instagram.com/whiterabbitmoscow',
+      subway: 'Смоленская',
+    },
+    {
+      name: 'Remy Kitchen Bakery',
+      comment: 'Лига дорогобогато/когда-нибудь попробовать',
+      href: 'https://instagram.com/remykitchenbakery',
+      subway: 'Деловой центр, Арбатская/Пушкинская, Менделеевская',
+    },
+    {
+      name: 'Сахалин',
+      comment: 'Лига дорогобогато/когда-нибудь попробовать',
+      href: 'https://sakhalin-moscow.ru',
+      subway: 'Смоленская',
+    },
+    {
+      name: 'Modus',
+      comment: 'Лига дорогобогато/когда-нибудь попробовать',
+      href: 'https://modus.moscow/',
+      subway: 'Киевская',
+    },
+    {
+      name: 'Butler',
+      comment: 'Лига дорогобогато/когда-нибудь попробовать',
+      href: 'https://www.instagram.com/butler.restaurant',
+      subway: 'Маяковская',
+    },
+  ]
+}
---
+++ script.js
@@ -0,0 +1,101 @@
+const btoi = (isAscending) => (isAscending ? 1 : -1)
+
+const getComparatorByName = (name, isAscending = true) => (a, b) =>
+  a[name] > b[name] ? btoi(isAscending) : -btoi(isAscending)
+
+const render = (comparator = null) => {
+  const data = getData()
+  if (comparator !== null) {
+    data.sort(comparator)
+  }
+
+  const rows = data.map(({ name, comment, href, subway }) => {
+    const tr = document.createElement('tr')
+
+    const tdName = document.createElement('td')
+    const nameLink = document.createElement('a')
+    nameLink.setAttribute('href', href)
+    nameLink.innerText = name
+    tdName.appendChild(nameLink)
+    tr.appendChild(tdName)
+
+    const tdComment = document.createElement('td')
+    tdComment.innerText = comment
+    tr.appendChild(tdComment)
+
+    const tdSubway = document.createElement('td')
+    tdSubway.innerText = subway
+    tr.appendChild(tdSubway)
+
+    return tr
+  })
+
+  const tbody = document.getElementById('tbody')
+  tbody.innerHTML = ''
+  rows.map((tr) => tbody.appendChild(tr))
+}
+
+const getNextSortOrder = (isAsc) => {
+  switch (isAsc) {
+    case true:
+      return false
+    case false:
+      return null
+    case null:
+      return true
+  }
+}
+
+const getSortOrderClass = (isAsc) => {
+  switch (isAsc) {
+    case true:
+      return 'sort-asc'
+    case false:
+      return 'sort-desc'
+    case null:
+      return ''
+  }
+}
+
+const SortStateCols = {
+  COL_NAME: 'name',
+  COL_COMMENT: 'comment',
+  COL_SUBWAY: 'subway',
+}
+
+let sortState = {
+  col: SortStateCols.COL_COMMENT,
+  isAscending: null,
+}
+
+render()
+
+const genEventListener = (name) => ({ currentTarget }) => {
+  if (sortState.col !== name) {
+    sortState = {
+      col: name,
+      isAscending: true,
+    }
+  } else {
+    sortState = {
+      col: name,
+      isAscending: getNextSortOrder(sortState.isAscending),
+    }
+  }
+
+  render(
+    sortState.isAscending !== null
+      ? getComparatorByName(sortState.col, sortState.isAscending)
+      : null,
+  )
+  currentTarget.setAttribute(
+    'class',
+    `sortable ${getSortOrderClass(sortState.isAscending)}`,
+  )
+}
+
+Object.values(SortStateCols).forEach((key) => {
+  document
+    .querySelector(`[data-key=${key}]`)
+    .addEventListener('click', genEventListener(key))
+})
